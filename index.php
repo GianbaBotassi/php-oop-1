3a6bd05bfdda53fb0a99e1ed8d29ff859e680db8
GianbaBotassi <gianba89@gmail.com>
2023-06-30 14:39:43 +0200

    Print array genres with method inside getMovieInfo method

--- index.php
+++ index.php
@@ -4,33 +4,48 @@ class Movie {
   
   public $title;
   public $year;
+  public $director;
   public $time;
   public $main_actor;
-  public $genre;
+  public $genres;
   
-  public function __construct($title, $year, $time, $main_actor, $genre){
+  public function __construct($title, $year, $director, $time, $main_actor, ...$genres){
 
     $this -> title = $title;
     $this -> year = $year;
+    $this -> director = $director;
     $this -> time= $time;
     $this -> main_actor = $main_actor;
-    $this -> genre = $genre;
+    $this -> genres = $genres;
 
+  }
+
+  public function cycleGenres(){
+    foreach($this->genres as $genre ){
+      $generi .= "<li>" .$genre ."</li>";
+    }
+    return $generi;
   }
 
   public function getMovieInfo(){
 
-    return "Titolo: " .$this -> title ."<br>Genere: " .$this -> genre ."<br>Anno: " .$this -> year ."<br>Attore principale: " .$this -> main_actor;
+    $info = "Titolo: " .$this -> title 
+    ."<br>Anno: " .$this -> year 
+    ."<br>Regista: " .$this -> director 
+    ."<br>Attore principale: " .$this -> main_actor;
+    $info .="<br>Genere: <ul>" .$this -> cycleGenres() ."</ul>";
+
+    return $info;
   }
 
   
 };
 
-$movie1 = new Movie("Into the Wild", "2007","148 min","Emile Hirsch", "Sean Penn");
-$movie2 = new Movie("The Dawn Wall", "2017", "100 min", "Tommy Caldwell", "Josh Lowell");
+$movie1 = new Movie("Into the Wild", "2007","Sean Penn", "148 min","Emile Hirsch", "Drammatico", "Biografico", "Avventura");
+$movie2 = new Movie("The Dawn Wall", "2017", "Josh Lowell", "100 min", "Tommy Caldwell", "Documentario", "Sportivo");
 
 
-
+// echo "<pre>",var_dump($movie1),"</pre>";
 
 
 echo $movie1 -> getMovieInfo();
